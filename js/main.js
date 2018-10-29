f1570c19aa927a7a38fa20325c75f9261c167d5b
Jose Daniel <Jodacam2@gmail.com>
2018-10-29 12:13:15 +0100

    Añadido meter y quitar objectos

--- js/main.js
+++ js/main.js
@@ -30,22 +30,34 @@ var gameObjects;
 window.addEventListener("load",function(ev){
     canvasManager = new CanvasManager("gameCanvas",1280,720);
     canvasManager.ClearCanvas();
+    LoadObjects(ev);
     StartGame();
 })
+window.addEventListener("keydown",function(ev){
+    if(ev.key=="e"){
+        canvasManager.ClearCanvas();
+    }else{
+        canvasManager.AddList(gameObjects);
+    }
+})
 
-
-function LoadObjects(ev){}
+function LoadObjects(ev){
+    gameObjects = [];
+    for (let i = 0; i < 5; i++) {
+        gameObjects[i] = [];
+    }
+    let timmy = new SpriteObject("player", new Vector2(50,350),"none",300,300);
+    let back = new SpriteObject("a",new Vector2(0,0),"assets/img/back.jpeg",720,3000);
+    let animation = new Animation("assets/img/Timmy_spritesheet.png",8,138,400,0.1);
+    back.velocity = new Vector2(-100,0);
+    timmy.AddAnimation(animation,"idle");
+    timmy.SetAnimation("idle");
+    gameObjects[2].push(timmy);
+    gameObjects[0].push(back);
+}
 function LoadLevel(){}
 function StartGame(){
     canvasManager.ClearCanvas();
-    let timmy = new SpriteObject("player", new Vector2(50,350),"none",300,300);
-    let animation = new Animation("assets/img/Timmy_spritesheet.png",8,138,400,0.1);
-    let back = new SpriteObject("a",new Vector2(0,0),"assets/img/back.jpeg",720,3000);
-    back.velocity = new Vector2(-100,0);
-    canvasManager.AddObject(back,0);
-    //timmy.velocity= new Vector2(100,0);
-    timmy.AddAnimation(animation,"idle");
-    timmy.SetAnimation("idle");
-    canvasManager.AddObject(timmy,4);
+    canvasManager.AddList(gameObjects);
     canvasManager.Start();
 }
