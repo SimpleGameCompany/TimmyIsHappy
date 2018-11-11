e770c521f1c7cb23bf40fb7e6447b3386d5b0b15
pabloski300 <pabloski300@hotmail.com>
2018-11-11 11:39:08 +0100

    Fondos scrolleables y cambiado el nombre de clases y variables a ingles

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-class Obstacule extends HitableObject{
+class Obstacle extends HitableObject{
     /**
      * 
      * @param {String} name 
@@ -7,18 +7,18 @@ class Obstacule extends HitableObject{
      * @param {String} imgSrc 
      * @param {number} height 
      * @param {number} width 
-     * @param {SpriteObject} jugador
+     * @param {SpriteObject} player
      
      */
-    constructor(name, position, imgSrc, height, width,jugador, hitnumber){
+    constructor(name, position, imgSrc, height, width,player, hits){
         super(name, position, imgSrc, height, width);
-        this._player = jugador;
-        this._hitnumber = hitnumber; 
+        this._player = player;
+        this._hits = hits; 
     }
 
     OnClick(e){
-        this._hitnumber --;
-        if(this._hitnumber == 0){
+        this._hits --;
+        if(this._hits == 0){
             this.deactivate();
             canvasManager.clickObjects.delete(this.hitColor);
             this.SetAnimation("clicked");
@@ -26,34 +26,24 @@ class Obstacule extends HitableObject{
     }
 }
 
-class Alcantarilla extends Obstacule{
-    constructor(name, position, imgSrc, height, width, jugador, hitnumber){
-        super(name, position, imgSrc, height, width, jugador, hitnumber);
+class Sewer extends Obstacle{
+    constructor(name, position, imgSrc, height, width, player, hits){
+        super(name, position, imgSrc, height, width, player, hits);
     }
 }
 
-class ObjetoVolador extends Obstacule{
-    constructor(name, position, imgSrc, height, width, jugador, hitnumber){
-        super(name, position, imgSrc, height, width, jugador, hitnumber);
+class FlyingObject extends Obstacle{
+    constructor(name, position, imgSrc, height, width, player, hits){
+        super(name, position, imgSrc, height, width, player, hits);
     }
     OnClick(e){
         super.OnClick(e);
     }
 }
 
-class Caca extends Obstacule{
-    constructor(name, position, imgSrc, height, width, jugador, hitnumber){
-        super(name, position, imgSrc, height, width, jugador, hitnumber);
-    }
-
-    OnClick(e){
-        super.OnClick(e);
-    }
-}
-
-class Paloma extends Obstacule{
-    constructor(name, position, imgSrc, height, width, jugador, hitnumber){
-        super(name, position, imgSrc, height, width, jugador, hitnumber);
+class Caca extends Obstacle{
+    constructor(name, position, imgSrc, height, width, player, hits){
+        super(name, position, imgSrc, height, width, player, hits);
     }
 
     OnClick(e){
@@ -61,9 +51,9 @@ class Paloma extends Obstacule{
     }
 }
 
-class Coche extends Obstacule{
-    constructor(name, position, imgSrc, height, width, jugador, hitnumber){
-        super(name, position, imgSrc, height, width, jugador, hitnumber);
+class Dove extends Obstacle{
+    constructor(name, position, imgSrc, height, width, player, hits){
+        super(name, position, imgSrc, height, width, player, hits);
     }
 
     OnClick(e){
@@ -71,10 +61,20 @@ class Coche extends Obstacule{
     }
 }
 
-class Perro extends Obstacule{
-    constructor(name, position, imgSrc, height, width, jugador, hitnumber, ladridos){
-        super(name, position, imgSrc, height, width, jugador, hitnumber);
-        this._ladridos = ladridos;
+class Car extends Obstacle{
+    constructor(name, position, imgSrc, height, width, player, hits){
+        super(name, position, imgSrc, height, width, player, hits);
+    }
+
+    OnClick(e){
+        super.OnClick(e);
+    }
+}
+
+class Dog extends Obstacle{
+    constructor(name, position, imgSrc, height, width, player, hits, barkNum){
+        super(name, position, imgSrc, height, width, player, hits);
+        this._bark = barkNum;
         this._stopped = false;
         this.interval;
         this._dead = false;
@@ -82,18 +82,17 @@ class Perro extends Obstacule{
 
     OnClick(e){
         super.OnClick(e);
-        if(this._hitnumber == 0){
+        if(this._hits == 0){
             if(this.interval){
                 clearInterval(this.interval);
             }
-            //this.velocity = new Vector2(speed+65,0);
             this._dead = true;
         }
         
     }
 
     Update(timeDelta, hitbox){
-        if(this._ladridos == 0 && !this._dead){
+        if(this._bark == 0 && !this._dead){
             clearInterval(this.interval);
             this.velocity = new Vector2(speed+65,0);
         }else if(this.position.x <= 1200 && !this._stopped && !this._dead){
@@ -105,8 +104,41 @@ class Perro extends Obstacule{
 
     Ladrar() {
         this.SetAnimation("ladrar");
-        this._ladridos--;
+        this._bark--;
         console.log("Guau");
+    }
+}
+
+class Scrollable{
+    constructor(name, position, img, height, width, animName, frames, vel){
+        this.active = true;
+        this.b1 = new SpriteObject(name, position, img, height, width);
+        this.b1.velocity = new Vector2(vel,0);
+        let position2 = position.add(new Vector2(width,0));
+        this.b2 = new SpriteObject(name, position2, img, height, width);
+        this.b2.velocity = new Vector2(vel,0);
+        let anim = new Animation(animName,frames,width,height,1/frames);
+
+        this.b1.AddAnimation(anim,"idle");
+        this.b1.SetAnimation("idle");
+        this.b2.AddAnimation(anim,"idle");
+        this.b2.SetAnimation("idle");
+    }
+
+    Update(deltaTime, hitBox){
+        this.b1.Update(deltaTime, hitBox);
+        this.b2.Update(deltaTime, hitBox);
+
+        if(this.b1.position.x <= -this.b1.width){
+            this.b1.position.x = this.b2.position.x+this.b2.width;
+        }else if(this.b2.position.x <= -this.b2.width){
+            this.b2.position.x = this.b1.position.x+this.b1.width;
+        }
+    }
+
+    Render(renderCanvas){
+        this.b1.Render(renderCanvas);
+        this.b2.Render(renderCanvas);
     }
 }
 
@@ -144,81 +176,68 @@ function LoadObjects(ev){
     }
 
     timmy = new SpriteObject("player", new Vector2(70,525),"none",205,138);
-    let animation = new Animation("assets/img/Timmy_spritesheet.png",8,138,205,0.1);
+    let animation = new Animation("assets/img/Timmy_spritesheet.png",8,138,205,1/8);
     timmy.AddAnimation(animation,"idle");
     timmy.SetAnimation("idle");
     timmy.anchor = new Vector2(0.5,0.5);
     gameObjects[2].push(timmy);
 
-    let backSun = new SpriteObject("backSun",new Vector2(0,0),"none",720,1280);
-    let backSunAnim = new Animation("assets/img/Sol_spritesheet.png",8,1280,720,0.5);
-    backSun.velocity = new Vector2(0,0);
-    backSun.AddAnimation(backSunAnim,"idle");
-    backSun.SetAnimation("idle");
-    gameObjects[0].push(backSun);
+    let sun = new SpriteObject("sun",new Vector2(0,0),"none",720,1280);
+    let sunAnim = new Animation("assets/img/Sol_spritesheet.png",8,1280,720,1/8);
+    sun.velocity = new Vector2(0,0);
+    sun.AddAnimation(sunAnim,"idle");
+    sun.SetAnimation("idle");
+    gameObjects[0].push(sun);
 
-    let backClouds = new SpriteObject("backClouds",new Vector2(0,0),"none",720,2560);
-    let backCloudsAnim = new Animation("assets/img/Nubes_spritesheet.png",4,2560,720,0.5);
-    backClouds.velocity = new Vector2(speed/8,0);
-    backClouds.AddAnimation(backCloudsAnim,"idle");
-    backClouds.SetAnimation("idle");
-    gameObjects[0].push(backClouds);
+    let clouds = new Scrollable("clouds",new Vector2(0,0),"none",720,2560,"assets/img/Nubes_spritesheet.png",4,speed/8);
+    gameObjects[0].push(clouds);
 
-    let backMountains = new SpriteObject("backMountains",new Vector2(0,0),"none",720,5120);
-    let backMountainsAnim = new Animation("assets/img/Fondo_spritesheet.png",3,5120,720,0.5);
-    backMountains.velocity = new Vector2(speed/2,0);
-    backMountains.AddAnimation(backMountainsAnim,"idle");
-    backMountains.SetAnimation("idle");
-    gameObjects[0].push(backMountains);
+    let mountains = new Scrollable("mountains",new Vector2(0,0),"none",720,5120,"assets/img/Fondo_spritesheet.png",3,speed/2);
+    gameObjects[0].push(mountains);
 
-    let backAcera = new SpriteObject("backAcera",new Vector2(0,0),"none",720,1280);
-    let backAceraAnim = new Animation("assets/img/Aceras_spritesheet.png",4,1280,720,0.5);
-    backAcera.velocity = new Vector2(speed,0);
-    backAcera.AddAnimation(backAceraAnim,"idle");
-    backAcera.SetAnimation("idle");
-    gameObjects[0].push(backAcera);
+    let sidewalks = new Scrollable("sidewalks",new Vector2(0,0),"none",720,1280,"assets/img/Aceras_spritesheet.png",4,speed);
+    gameObjects[0].push(sidewalks);
 
-    let backCarretera = new SpriteObject("backCarretera",new Vector2(0,0),"none",720,1280);
-    let backCarreteraAnim = new Animation("assets/img/Carretera_spritesheet.png",4,1280,720,0.5);
-    backCarretera.velocity = new Vector2(speed,0);
-    backCarretera.AddAnimation(backCarreteraAnim,"idle");
-    backCarretera.SetAnimation("idle");
-    gameObjects[0].push(backCarretera);
+    let road = new Scrollable("road",new Vector2(0,0),"none",720,1280,"assets/img/Carretera_spritesheet.png",4,speed);
+    gameObjects[0].push(road);
 }
 
 function LoadLevel(jsonName,container){
     $.getJSON("assets/files/"+jsonName+".json", function (json) {
         for(var obj of json){
             switch(obj.type){
-                case "perro":
-                    let perro = new Perro(obj.name,new Vector2(obj.positionx,obj.positiony),"none",obj.height,obj.width,timmy,obj.hitnumber, obj.ladridos);
-                    perro.anchor = new Vector2(0.5,0.5);
-                    perro.velocity = new Vector2(speed,0);
-                    container[obj.layer].push(perro);
+                case "dog":
+                    let dog = new Dog(obj.name,new Vector2(obj.positionx,obj.positiony),"none",obj.height,obj.width,timmy,obj.hitnumber, obj.ladridos);
+                    let dogRunning = new Animation("assets/img/PerroCorriendo_spritesheet.png",8,557,184,0.1);
+                    dog.anchor = new Vector2(0.5,0.5);
+                    dog.velocity = new Vector2(speed,0);
+                    dog.AddAnimation(dogRunning,"run");
+                    dog.SetAnimation("run");
+                    container[obj.layer].push(dog);
                 break;
-                case "alcantarilla":
-                    let alcantarilla = new Alcantarilla(obj.name,new Vector2(obj.positionx,obj.positiony),"none",obj.height,obj.width,timmy,obj.hitnumber);
-                    alcantarilla.anchor = new Vector2(0.5,0.5);
-                    alcantarilla.velocity = new Vector2(speed,0);
-                    container[obj.layer].push(alcantarilla);
+                case "sewer":
+                    let sewer = new Sewer(obj.name,new Vector2(obj.positionx,obj.positiony),"assets/img/alcantarilla.png",obj.height,obj.width,timmy,obj.hitnumber);
+                    sewer.anchor = new Vector2(0.5,0.5);
+                    sewer.velocity = new Vector2(speed,0);
+                    container[obj.layer].push(sewer);
                 break;
-                case "coche":
-                    let coche = new Coche(obj.name,new Vector2(obj.positionx,obj.positiony),"none",obj.height,obj.width,timmy,obj.hitnumber);
-                    let cocheAnim = new Animation("assets/img/Coche_spritesheet.png",8,557,184,0.1);
-                    coche.anchor = new Vector2(0.5,0.5);
-                    coche.velocity = new Vector2(speed - 20,0);
-                    coche.AddAnimation(cocheAnim,"idle");
-                    coche.SetAnimation("idle");
-                    container[obj.layer].push(coche);
+                case "car":
+                    let car = new Car(obj.name,new Vector2(obj.positionx,obj.positiony),"none",obj.height,obj.width,timmy,obj.hitnumber);
+                    let carAnim = new Animation("assets/img/Coche_spritesheet.png",8,557,184,0.1);
+                    car.anchor = new Vector2(0.5,0.5);
+                    car.velocity = new Vector2(speed - 20,0);
+                    car.AddAnimation(carAnim,"idle");
+                    car.SetAnimation("idle");
+                    container[obj.layer].push(car);
                 break;
-                case "paloma":
-                    let paloma = new Paloma(obj.name,new Vector2(obj.positionx,obj.positiony),"none",obj.height,obj.width,timmy,obj.hitnumber);
-                    let PalomaAnim = new Animation("none",8,557,184,0.1);
-                    paloma.anchor = new Vector2(0.5,0.5);
-                    paloma.velocity = new Vector2(speed - 20,0);
-                    paloma.AddAnimation(palomaAnim,"idle");
-                    paloma.SetAnimation("idle");
-                    container[obj.layer].push(paloma);
+                case "dove":
+                    let dove = new Dove(obj.name,new Vector2(obj.positionx,obj.positiony),"none",obj.height,obj.width,timmy,obj.hitnumber);
+                    let doveAnim = new Animation("none",8,557,184,0.1);
+                    dove.anchor = new Vector2(0.5,0.5);
+                    dove.velocity = new Vector2(speed - 20,0);
+                    dove.AddAnimation(doveAnim,"idle");
+                    dove.SetAnimation("idle");
+                    container[obj.layer].push(dove);
                 break;
             }
         }
