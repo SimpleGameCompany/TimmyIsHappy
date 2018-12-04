839a4fd71c4f69f2b08ec5aba419ba333c62fc6c
pabloski300 <32078402+pabloski300@users.noreply.github.com>
2018-12-04 16:12:42 +0100

    Flecha Anim en los menus
    Animacion de la flecha en menu opciones y scores

--- js/main.js
+++ js/main.js
@@ -623,7 +623,11 @@ function OptionsMenu(){
         }
 
         let flecha;
-        flecha = new HitableObject("continuar", new Vector2(130,570),"assets/img/Flecha.png",52,106);
+        flecha = new HitableObject("continuar", new Vector2(130,570),"none",52,106);
+        let flechaAnim;
+        flechaAnim = new Animation("assets/img/Flecha_spritesheet.png",4,106,52,1/8,0);
+        flecha.AddAnimation(flechaAnim,"idle");
+        flecha.SetAnimation("idle");
         flecha.OnClick = function(ev){
             soundManager.PlayButton1();
             if(fondoMenuOptionsEsp)
@@ -766,6 +770,10 @@ function PuntuacionesMenu(){
             }
         }
         let flecha = new HitableObject("continuar", new Vector2(130,570),"assets/img/Flecha.png",52,106);
+        let flechaAnim;
+        flechaAnim = new Animation("assets/img/Flecha_spritesheet.png",4,106,52,1/8,0);
+        flecha.AddAnimation(flechaAnim,"idle");
+        flecha.SetAnimation("idle");
         flecha.OnClick = function(ev){
             soundManager.PlayButton1();
             if(fondoMenuScoresEsp)
